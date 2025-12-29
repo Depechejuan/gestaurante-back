be79aa8f659b2df306c5f83f9d8438c313c05adc
Juan León <depechejuan@gmail.com>
2025-12-29 15:43:21 +0100

    Creacion base DTO y Empleados

---
+++ Gestaurante/Models/DTO/CrearEmpleadoDTO.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Gestaurante.Models.DTO
+{
+    public enum TipoEmpleado
+    {
+        Camarero,
+        Cocinero,
+        Administrador
+    }
+
+    public class CrearEmpleadoDTO
+    {
+        [Required]
+        public TipoEmpleado Tipo { get; set; }
+
+        [Required]
+        public string Email { get; set; } = string.Empty;
+
+        public string FirstName { get; set; } = string.Empty;
+        public string FirstLastName { get; set; } = string.Empty;
+        public string SecondLastName { get; set; } = string.Empty;
+
+        public string DNI { get; set; } = string.Empty;
+        public string NUSS { get; set; } = string.Empty;
+    }
+}
