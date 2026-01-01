4e32479d16a8eb12387b551f92e6dca7516d72d9
Juan León <depechejuan@gmail.com>
2026-01-01 13:58:38 +0100

    cambios para poder obtener respuseta en admin/register

--- Gestaurante/Models/Entities/Cocinero.cs
+++ Gestaurante/Models/Entities/Cocinero.cs
@@ -4,17 +4,19 @@ namespace Gestaurante.Models.Entities
 {
     public class Cocinero : Empleado
     {
-        public Cocinero(RegistroDTO dto)
+        public Cocinero() { }
+        public Cocinero(Guid id, string email, string password, string firstName,
+            string firstLastName, string secondLastName, string dni, string nuss, DateTime createdAt)
         {
-            this.Id = dto.Id;
-            this.Email = dto.Email;
-            this.Password = dto.Password;
-            this.FirstName = dto.FirstName;
-            this.FirstLastName = dto.FirstLastName;
-            this.SecondLastName = dto.SecondLastName;
-            this.DNI = dto.DNI;
-            this.NUSS = dto.NUSS;
-            this.CreatedAt = dto.CreatedAt;
+            this.Id = id;
+            this.Email = email;
+            this.Password = password;
+            this.FirstName = firstName;
+            this.FirstLastName = firstLastName;
+            this.SecondLastName = secondLastName;
+            this.DNI = dni;
+            this.NUSS = nuss;
+            this.CreatedAt = createdAt;
         }
 
         public void CompletarPlato()
--- Gestaurante/Models/Entities/Empleado.cs
+++ Gestaurante/Models/Entities/Empleado.cs
@@ -8,14 +8,30 @@ namespace Gestaurante.Models.Entities
     {
         public Guid Id { get; protected set; }
 
+        [Required]
+        [MaxLength(100)]
         public string Email { get; protected set; } = string.Empty;
+        [Required]
+        [MinLength(8)]
+        [MaxLength(255)]
         public string Password { get; protected set; } = string.Empty;
 
+        [Required]
+        [MaxLength(150)]
         public string FirstName { get; protected set; } = string.Empty;
+
+        [Required]
+        [MaxLength(100)]
         public string FirstLastName { get; protected set; } = string.Empty;
+
+        [Required]
+        [MaxLength(100)]    
         public string SecondLastName { get; protected set; } = string.Empty;
 
+        [Required]
+        [MinLength(9)]
         public string DNI { get; protected set; } = string.Empty;
+        [MinLength(13)]
         public string NUSS { get; protected set; } = string.Empty;
 
         public DateTime CreatedAt { get; protected set; } = DateTime.UtcNow;
