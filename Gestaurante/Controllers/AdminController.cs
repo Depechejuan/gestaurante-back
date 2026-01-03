1baa6b04a694304b7d72ceb967ca2e3f71b0e8c0
Juan León <depechejuan@gmail.com>
2026-01-03 22:00:09 +0100

    endpoint recepcion/respuesta de usuario por ID

--- Gestaurante/Controllers/AdminController.cs
+++ Gestaurante/Controllers/AdminController.cs
@@ -13,12 +13,13 @@ namespace Gestaurante.Controllers
     public class AdminController : ControllerBase
     {
         private readonly RegisterService _registerService;
+        private readonly StaffService _staffService;
 
-        public AdminController(RegisterService registerService)
+        public AdminController(RegisterService registerService, StaffService staffService)
         {
             _registerService = registerService;
+            _staffService = staffService;
         }
-
 
         [HttpPost("register")]
         public IActionResult Register([FromBody] RegistroDTO dto)
@@ -26,7 +27,25 @@ namespace Gestaurante.Controllers
             try
             {
                 var empleado = _registerService.CrearEmpleado(dto);
-                return ResponseHelper.SendResponse( new { id = empleado.Id });
+                return ResponseHelper.SendResponse(new { id = empleado.Id });
+            }
+            catch (Exception ex)
+            {
+                return ResponseHelper.SendError(ex, 500);
+            }
+        }
+
+        [HttpPost("getbasicuser")]
+        public IActionResult GetBasicUser([FromBody] IdRequestDTO user)
+        {
+            try
+            {
+                var empleado = _staffService.GetBasicStaff(user.Id);
+                if (empleado == null)
+                {
+                    throw new Exception("Empleado no encontrado");
+                }
+                return ResponseHelper.SendResponse(empleado);
             }
             catch (Exception ex)
             {
