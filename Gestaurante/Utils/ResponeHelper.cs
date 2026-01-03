0af163a26bdac70f5f22e2856cb841d834baf9da
Juan León <depechejuan@gmail.com>
2026-01-03 18:18:04 +0100

    Envio de respuestas personalizadas de la API

---
+++ Gestaurante/Utils/ResponeHelper.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Gestaurante.Utils
+{
+    public static class ResponseHelper
+    {
+        public static IActionResult SendResponse(object? data, int status = 200)
+        {
+            var response = new
+            {
+                status,
+                data
+            };
+
+            return new ObjectResult(response) { StatusCode = status };
+        }
+
+
+        public static IActionResult SendError(object? error, int status = 400)
+        {
+            var response = new
+            {
+                status,
+                error
+            };
+
+            return new ObjectResult(response) { StatusCode = status };
+        }
+    }
+}
