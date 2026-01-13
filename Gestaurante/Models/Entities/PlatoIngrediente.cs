b077197243f018baeca1a82c22f2815cd3e48d8a
CarlosEspEsc <cespinosaescrig@gmail.com>
2026-01-13 12:34:56 +0100

    añadidas tablas intermedias, pero aun falta añadir relaciones y campos necesarios para la conexion entre tablas

---
+++ Gestaurante/Models/Entities/PlatoIngrediente.cs
@@ -0,0 +1,25 @@
+﻿namespace Gestaurante.Models.Entities
+{
+    public class PlatoIngrediente
+    {
+        [Required]
+        [ForeignKey("Plato")]
+        public Guid PlatoId { get; set; }
+
+        [Required]
+        [ForeignKey("Ingrediente")]
+        public Guid IngredienteId { get; set; }
+
+            // Propiedades de navegación
+        public virtual Plato Plato { get; set; } = null!;
+        public virtual Ingrediente Ingrediente { get; set; } = null!;
+
+        public PlatoIngrediente() { }
+
+        public PlatoIngrediente(Guid platoId, Guid ingredienteId)
+        {
+            PlatoId = platoId;
+            IngredienteId = ingredienteId;
+        }
+    }
+}
