58110ce980ea041b56abd0d09ea4bbd8b6d8e699
CarlosEspEsc <cespinosaescrig@gmail.com>
2026-02-03 15:18:02 +0100

    commit para creacion de nueva rama

---
+++ Gestaurante/Controllers/PlatoController.cs
@@ -0,0 +1,51 @@
+﻿using Gestaurante.Models.DTO;
+using Gestaurante.Models.Entities;
+using Gestaurante.Models.Services;
+using Gestaurante.Utils;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+namespace Gestaurante.Controllers
+{
+
+        [ApiController]
+        [Route("[controller]")]
+        public class PlatoController : ControllerBase
+        {
+        [HttpGet("getall")]
+        public IActionResult GetAllPlatos()
+        {
+            var platos = new List<Plato>
+                {
+                    new Plato {  Nombre = "Spaghetti Carbonara", Descripcion = "Pasta con salsa de huevo, queso y panceta.", Precio = 12.99M },
+                    new Plato {  Nombre = "Margherita Pizza", Descripcion = "Pizza clásica con tomate, mozzarella y albahaca.", Precio = 10.99M },
+                    new Plato {  Nombre = "Caesar Salad", Descripcion = "Ensalada con lechuga romana, crutones y aderezo César.", Precio = 8.99M }
+                };
+                return ResponseHelper.SendResponse(platos);
+        
+        }
+        //private readonly Plato _platoService;
+        //public PlatoController(Plato platoService)
+        //{
+        //    _platoService = platoService;
+        //}
+        //[HttpGet("getall")]
+        //public IActionResult GetAllPlatos()
+        //{
+        //    try
+        //    {
+        //        var platos = _platoService.GetAllPlatos();
+        //        return ResponseHelper.SendResponse(platos);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return ResponseHelper.SendError(new
+        //        {
+        //            message = ex.Message,
+        //            detail = ex.InnerException?.Message
+        //        }, 500);
+        //    }
+        //}
+    }
+    
+}
