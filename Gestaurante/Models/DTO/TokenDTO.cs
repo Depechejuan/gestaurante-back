6f04ed14c30c58bc2da2c1a418cf15566658be8b
Juan León <depechejuan@gmail.com>
2026-02-16 20:26:16 +0100

    quick fix en develop, devolucion tipo user en login

--- Gestaurante/Models/DTO/TokenDTO.cs
+++ Gestaurante/Models/DTO/TokenDTO.cs
@@ -5,12 +5,14 @@
         public string Token { get; set; }
         public DateTime ExpiraEn { get; set; }
         public Guid Id { get; set; }
+        public TipoEmpleado Tipo { get; set; }
 
-        public TokenDTO(string token, DateTime expire, Guid id)
+        public TokenDTO(string token, DateTime expire, Guid id, TipoEmpleado tipo)
         {
             this.Token = token;
             this.ExpiraEn = expire;
             this.Id = id;
+            this.Tipo = tipo;
         }
     }
 }
